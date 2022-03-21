915b49657bda02fd29d9b168009bca0411024421
Joseph Kowalski <jkjr25@gmail.com>
2022-03-21 13:59:10 -0400

    Working on exercise 2

--- gradebook.py
+++ gradebook.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from uuid import uuid4
 
 class AliveStatus(Enum):
     Deceased = 0
@@ -23,4 +24,27 @@ class Person:
     def update_status(self, alive):
         self.alive = alive
 
+class Instructor(Person):
+    def __init__(self, first_name, last_name, dob, alive):
+        super().__init__(first_name, last_name, dob, alive)
+        self.instructor_id = f'Instructor_{uuid4()}'
 
+class Student(Person):
+    def __init__(self, first_name, last_name, dob, alive):
+        super().__init__(first_name, last_name, dob, alive)
+        self.student_id = f'Student_{uuid4()}'
+
+class ZipCodeStudent(Student):
+    def __init__(self, first_name, last_name, dob, alive, student_id):
+        super().__init__(first_name, last_name, dob, alive, student_id)
+
+class CollegeStudent(Student):
+    def __init__(self, first_name, last_name, dob, alive, student_id):
+        super().__init__(first_name, last_name, dob, alive, student_id)
+
+class Classroom:
+    def __init__(self):
+        self.students = []
+        self.instructors = []
+
+    def add_instructor(self, instructor):
